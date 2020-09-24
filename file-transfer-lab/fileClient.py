f10a4d9e392b9495ccf4e49f06313be16be2045a
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-24 10:50:01 -0600

    asks user what file to send, then sends it line by line, first draft

---
+++ file-transfer-lab/fileClient.py
@@ -0,0 +1,62 @@
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from framedSock import framedSend, framedReceive
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+s = socket.socket(addrFamily, socktype)
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+s.connect(addrPort)
+
+user_file = input("What file would you like to send? ")
+f = open(user_file, "r")
+for line in f:
+    print("sending "+line)
+    framedSend(s, str.encode(line), debug)
+    print("received:", framedReceive(s, debug))
+
+f.close()
+#print("sending hello world")
+#framedSend(s, b"hello world", debug)
+#print("received:", framedReceive(s, debug))
+#
+#print("sending hello world")
+#framedSend(s, b"hello world", debug)
+#print("received:", framedReceive(s, debug))
+
